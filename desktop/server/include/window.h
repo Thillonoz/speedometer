35d7eb380869c2de88c2953b114a20ceece420c6
Khanh Tran <88000687+YunruKnowledge@users.noreply.github.com>
2025-06-03 14:26:01 +0200

    Resolves #2 Server GUI (No communication)

--- desktop/server/include/window.h
+++ desktop/server/include/window.h
@@ -2,9 +2,61 @@
 #define WINDOW_H
 
 #include <QDialog>
+#include <QFormLayout>
+#include <QString>
+#include <QVBoxLayout>
 
-class Window : public QDialog
-{
+#include <QCheckBox>
+#include <QLabel>
+#include <QSlider>
+
+#define SPEED_SUFFIX " Km/h"
+#define TEMPERATURE_SUFFIX " °C"
+#define BATTERY_SUFFIX " %"
+
+#define SLIDER_WIDTH (300)
+#define SLIDER_LABEL_CHAR_SIZE 8
+#define CALC_LABEL_WIDTH(x) (8 * (x + 1))
+
+class Window : public QDialog {
+  // --- temp
+  int speedValue{0};
+  int temperatureValue{0};
+  int BatteryLevelValue{0};
+  enum indicatorValue {
+    none,
+    left,
+    right,
+    warning
+  } indicatorValue{indicatorValue::none};
+  // ---
+
+  QVBoxLayout mainLayout;
+  QFormLayout formLayout;
+
+  QBoxLayout speedSlideBarLayout{QBoxLayout::Direction::LeftToRight};
+  QBoxLayout temperatureSlideBarLayout{QBoxLayout::Direction::LeftToRight};
+  QBoxLayout batteryLevelSlideBarLayout{QBoxLayout::Direction::LeftToRight};
+  QLabel speedLabel, temperatureLabel, batteryLevelLabel;
+  QSlider speedSlider{Qt::Orientation::Horizontal};
+  QSlider temperatureSlider{Qt::Orientation::Horizontal};
+  QSlider batteryLevelSlider{Qt::Orientation::Horizontal};
+
+  QBoxLayout indicatorLayout{QBoxLayout::Direction::LeftToRight};
+  QLabel indicatorLeftLabel, indicatorRightLabel, indicatorWarningLabel;
+  QCheckBox indicatorLeft;
+  QCheckBox indicatorRight;
+  QCheckBox indicatorWarning;
+
+  void onSlideSpeed(int);
+  void onSlideTemperature(int);
+  void onSlideBatteryLevel(int);
+  void onClickLeftIndicator(int);
+  void onClickRightIndicator(int);
+  void onClickWarningIndicator(int);
+
+public:
+  Window();
 };
 
 #endif
--- desktop/server/src/window.cpp
+++ desktop/server/src/window.cpp
@@ -1,2 +1,134 @@
 #include "window.h"
 #include "setting.h"
+
+Window::Window() {
+  setWindowTitle("Server");
+  setLayout(&mainLayout);
+
+  // Layouts & size properties
+  mainLayout.addLayout(&formLayout);
+  mainLayout.setSizeConstraint(QLayout::SetFixedSize);
+
+  formLayout.addRow("Speed:", &speedSlideBarLayout);
+  formLayout.addRow("Temperature:", &temperatureSlideBarLayout);
+  formLayout.addRow("Battery level:", &batteryLevelSlideBarLayout);
+  formLayout.addRow("Light signals:", &indicatorLayout);
+  formLayout.setLabelAlignment(Qt::AlignmentFlag::AlignRight);
+
+  speedSlideBarLayout.addWidget(&speedSlider);
+  speedSlideBarLayout.addWidget(&speedLabel);
+  speedSlider.setFixedWidth(SLIDER_WIDTH);
+  speedLabel.setMinimumWidth(CALC_LABEL_WIDTH(SLIDER_LABEL_CHAR_SIZE));
+
+  temperatureSlideBarLayout.addWidget(&temperatureSlider);
+  temperatureSlideBarLayout.addWidget(&temperatureLabel);
+  temperatureSlider.setFixedWidth(SLIDER_WIDTH);
+  temperatureSlider.setMinimumWidth(CALC_LABEL_WIDTH(SLIDER_LABEL_CHAR_SIZE));
+
+  batteryLevelSlideBarLayout.addWidget(&batteryLevelSlider);
+  batteryLevelSlideBarLayout.addWidget(&batteryLevelLabel);
+  batteryLevelSlider.setFixedWidth(SLIDER_WIDTH);
+  batteryLevelSlider.setMinimumWidth(CALC_LABEL_WIDTH(SLIDER_LABEL_CHAR_SIZE));
+
+  indicatorLayout.addWidget(&indicatorLeft);
+  indicatorLayout.addWidget(&indicatorRight);
+  indicatorLayout.addWidget(&indicatorWarning);
+
+  // actions & value properties
+  QString startLabelText;
+  speedSlider.setMinimum(SPEEDMIN);
+  speedSlider.setMaximum(SPEEDMAX);
+  startLabelText.setNum(speedSlider.value());
+  startLabelText.append(SPEED_SUFFIX);
+  speedLabel.setText(startLabelText);
+  connect(&speedSlider, &QSlider::valueChanged, this, &Window::onSlideSpeed);
+
+  temperatureSlider.setMinimum(TEMPMIN);
+  temperatureSlider.setMaximum(TEMPDMAX);
+  startLabelText.setNum(temperatureSlider.value());
+  startLabelText.append(TEMPERATURE_SUFFIX);
+  temperatureLabel.setText(startLabelText);
+  connect(&temperatureSlider, &QSlider::valueChanged, this,
+          &Window::onSlideTemperature);
+
+  batteryLevelSlider.setMinimum(BATTMIN);
+  batteryLevelSlider.setMaximum(BATTMAX);
+  startLabelText.setNum(batteryLevelSlider.value());
+  startLabelText.append(BATTERY_SUFFIX);
+  batteryLevelLabel.setText(startLabelText);
+  connect(&batteryLevelSlider, &QSlider::valueChanged, this,
+          &Window::onSlideBatteryLevel);
+
+  indicatorLeft.setText("Left");
+  connect(&indicatorLeft, &QCheckBox::stateChanged, this,
+          &Window::onClickLeftIndicator);
+
+  indicatorRight.setText("Right");
+  connect(&indicatorRight, &QCheckBox::stateChanged, this,
+          &Window::onClickRightIndicator);
+
+  indicatorWarning.setText("Warning");
+  connect(&indicatorWarning, &QCheckBox::stateChanged, this,
+          &Window::onClickWarningIndicator);
+}
+
+void Window::onSlideSpeed(int _value) {
+  QString temp;
+  temp.setNum(_value);
+  temp.append(SPEED_SUFFIX);
+  speedLabel.setText(temp);
+
+  // Send communication?
+}
+
+void Window::onSlideTemperature(int _value) {
+  QString temp;
+  temp.setNum(_value);
+  temp.append(TEMPERATURE_SUFFIX);
+  temperatureLabel.setText(temp);
+
+  // Send communication?
+}
+
+void Window::onSlideBatteryLevel(int _value) {
+  QString temp;
+  temp.setNum(_value);
+  temp.append(BATTERY_SUFFIX);
+  this->batteryLevelLabel.setText(temp);
+
+  // Send communication?
+}
+
+void Window::onClickLeftIndicator(int _state) {
+  if (_state == Qt::CheckState::Checked) {
+    indicatorRight.setDisabled(true);
+    indicatorRight.setCheckState(Qt::CheckState::Unchecked);
+  } else {
+    indicatorRight.setDisabled(false);
+  }
+
+  if (indicatorWarning.checkState() == Qt::CheckState::Unchecked) {
+    // Send communication?
+  } else {
+    ;
+  }
+}
+
+void Window::onClickRightIndicator(int _state) {
+  if (_state == Qt::CheckState::Checked) {
+    indicatorLeft.setDisabled(true);
+    indicatorLeft.setCheckState(Qt::CheckState::Unchecked);
+  } else {
+    indicatorLeft.setDisabled(false);
+  }
+
+  if (indicatorWarning.checkState() == Qt::CheckState::Unchecked) {
+    // Send communication?
+  } else {
+    ;
+  }
+}
+
+void Window::onClickWarningIndicator(int _state) {
+  // Send communication?
+}
