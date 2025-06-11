e4a92eb193ac6ccda9fc3a6a8b2673f458d1b49e
Thillonoz <emilivarsson92@gmail.com>
2025-06-11 11:19:59 +0200

    Removed changes to tcpservice.h and main.cpp

--- desktop/client/include/tcpservice.h
+++ desktop/client/include/tcpservice.h
@@ -8,21 +8,8 @@
 
 class TCPService : public COMService
 {
-    int sockfd;
-    std::atomic<bool> end{false};
-    std::thread thrd{&TCPService::run, this};
-
-    void run(void) override;
 
 public:
-    TCPService() = default;
-    ~TCPService()
-    {
-        end = true;
-        shutdown(sockfd, SHUT_RDWR);
-        close(sockfd);
-        thrd.join();
-    }
 };
 
 #endif
--- desktop/client/main.cpp
+++ desktop/client/main.cpp
@@ -1,8 +1,3 @@
-#ifdef UARTCOM
-#include "uartservice.h"
-#else
-#include "tcpservice.h"
-#endif
 
 #include "window.h"
 #include <QApplication>
@@ -10,14 +5,6 @@
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-
-#ifdef UARTCOM
-    UARTService comService;
-#else
-    TCPService comService;
-#endif
-    Window window(comService);
-    window.show();
 
     return app.exec();
 }
