638b9f3bb89c07ee71990bb937901c9746e3efe9
Lazar <roksandiclazar@gmail.com>
2025-08-14 09:43:56 +0200

    Final fix, no dynamic memory allocation

--- desktop/server/include/uartservice.h
+++ desktop/server/include/uartservice.h
@@ -2,17 +2,13 @@
 #define UARTSERVICE_H
 
 #include <QThread>
-#include <QSerialPort>
 #include "comservice.h"
 
 class UARTService : public COMService, public QThread {
-    QSerialPort *serial;
     static constexpr char port_name[13] = "/dev/ttyUSB0";
 
 private:
     void run(void) override;
-
-    void set_serial_communication();
 
 public:
     UARTService() {
--- desktop/server/src/uartservice.cpp
+++ desktop/server/src/uartservice.cpp
@@ -1,3 +1,4 @@
+#include <QSerialPort>
 #include <QDebug>
 #include <mutex>
 
@@ -10,39 +11,36 @@
 static std::mutex mutex;
 
 void UARTService::run() {
-    serial = new QSerialPort();
-    set_serial_communication();
+    QSerialPort serial;
 
-    if (!serial->isOpen()) {
-        if (!serial->open(QSerialPort::WriteOnly)) {
+    serial.setPortName(port_name);
+    serial.setBaudRate(BAUDRATE);
+    serial.setDataBits(QSerialPort::Data8); // Sending one byte per frame
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+    serial.setFlowControl(QSerialPort::NoFlowControl);
+
+    if (!serial.isOpen()) {
+        if (!serial.open(QSerialPort::WriteOnly)) {
             qDebug() << "Failed to open serial port";
         }
     }
 
-    while (serial->isOpen()) {
+    while (serial.isOpen()) {
         mutex.lock();
         QByteArray data(reinterpret_cast<const char *>(buffer), BUFLEN);
 
-        serial->write(data);
-        serial->flush();
+        serial.write(data);
+        serial.flush();
 
         mutex.unlock();
 
         QThread::msleep(Setting::INTERVAL);
     }
 
-    if (serial->isOpen()) {
-        serial->close();
+    if (serial.isOpen()) {
+        serial.close();
     }
-}
-
-void UARTService::set_serial_communication() {
-    serial->setPortName(port_name);
-    serial->setBaudRate(BAUDRATE);
-    serial->setDataBits(QSerialPort::Data8); // Sending one byte per frame
-    serial->setParity(QSerialPort::NoParity);
-    serial->setStopBits(QSerialPort::OneStop);
-    serial->setFlowControl(QSerialPort::NoFlowControl);
 }
 
 UARTService::~UARTService() {
